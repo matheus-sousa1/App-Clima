0a86877b5124dec036000f2ea93e51e5fc523c39
matheus-sousa1 <matusalem3378@gmail.com>
2022-12-21 18:51:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
               <p>{data.name}</p>
             </div>
             <div className="temp">
-              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
             </div>
           </Top>
 
@@ -55,7 +55,7 @@ function App() {
             <Bottom>
               <div className="feels">
                 {data.main ? (
-                  <p className="bold">{data.main.feels_like.toFixed()}°C</p>
+                  <p className="bold">{data.main.feels_like.toFixed()}°F</p>
                 ) : null}
                 <p>Sensação</p>
               </div>
